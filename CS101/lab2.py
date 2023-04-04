445de9e70e9fa4e70772c7b93980f06ccff5b851
Hyeonseo Oh <hyeonseo.oh@stonybrook.edu>
2023-04-05 05:16:22 +0900

    lab2.

---
+++ CS101/lab2.py
@@ -0,0 +1,45 @@
+# -------------------------------------- Task 1 -----------------------------------
+def add(x, y):
+    return x + y
+
+# TODO: Add definitions of sub(), div(), mult(), exp(), as well as neg() and sqrt().
+#       neg() should return the negation of the given number, and sqrt() should
+#       return the square root of the given number. 
+def neg(x):
+    return -x
+
+def sqrt(x):
+    return x**(1/2)
+
+def sub(x,y):
+    return x-y
+
+def div(x,y):
+    return x/y
+
+def mult(x,y):
+    return x*y
+
+def exp(x):
+    return x**2
+
+# -------------------------------------- Task 2 -----------------------------------
+
+# TODO: Implement the quadratic formula using *only* the functions defined here.
+#       Do not use arithmetic operators directly. 
+#       You're allowed to define other functions.
+a = 1
+b = -3
+c = 1
+d = sub(exp(b), mult(4, mult(a,c)))
+
+
+# TODO: write a code to compute the first root of the quadratic equation
+x1 = div((add(neg(b), sqrt(d))),mult(2,a))
+# TODO: then do the same for the second root
+x2 = div((sub(neg(b),sqrt(d))),mult(2,a)) 
+
+# Note: Make sure to remove the ellipsis (...) when you're writing your code
+
+print("First root:" + str(x1))
+print("Second root:" + str(x2))
