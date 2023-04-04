f06e76b56d0db130d0a67c6fadb9ca9b33b286c7
Hyeonseo Oh <hyeonseo.oh@stonybrook.edu>
2023-04-05 05:36:56 +0900

    lab3
    lab3

---
+++ CS101/lab3.py
@@ -0,0 +1,44 @@
+# TODO: Complete this function for task 1
+# Use try-except block to handle errors
+def check_number1(s):
+
+    try:
+        if(len(s)>2):
+            s = int(s)
+            s%= 100
+            s = "0" + str(s)
+    except ValueError:
+        print("00")
+    return s
+
+# TODO: Complete this function for task 2
+# Use string methods to handle errors
+def check_number2(s):
+    if s.isnumeric():
+        if(len(s)>2):
+            s = int(S)
+            s%= 100
+            s = "0" + str(s)
+        return s
+    else:
+        print("00")
+
+def go():
+    year = input("Year of birth (YYYY): ")
+    month = input("Month of birth (MM): ")
+    day = input("Day of birth (DD): ")
+
+    y1 = check_number1(year)
+    m1 = check_number1(month)
+    d1 = check_number1(day)
+
+    print(y1 + m1 + d1) # Should print 020501 for 2002 May 1st
+
+    y2 = check_number2(year)
+    m2 = check_number2(month)
+    d2 = check_number2(day)
+
+    print(y2 + m2 + d2) # Should print 020501 for 2002 May 1st
+
+go()
+
